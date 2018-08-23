74166bb356b12980cf1cea8bf046aaf29ae4bd33
franciscofranco <franciscofranco.1990@gmail.com>
2018-08-23 15:21:01 +0400

    fsync: add a toggle to enable or disable fsync() operations

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2614,8 +2614,11 @@ extern int vfs_fsync(struct file *file, int datasync);
  * to already be updated for the write, and will return either the amount
  * of bytes passed in, or an error if syncing the file failed.
  */
+extern bool fsync_enabled; // fsync control
 static inline ssize_t generic_write_sync(struct kiocb *iocb, ssize_t count)
 {
+    if (!fsync_enabled)
+		return 0;
 	if (iocb->ki_flags & IOCB_DSYNC) {
 		int ret = vfs_fsync_range(iocb->ki_filp,
 				iocb->ki_pos - count, iocb->ki_pos - 1,
